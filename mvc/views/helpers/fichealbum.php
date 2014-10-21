16147e9ae0f7e81043436ae5cdd4fa7f9945aee1
Chninkel <chninkel@bdovore.com>
2014-10-21 21:14:39 +0200

    nettoyage et mise en page du code + suppression des doubles EOL

--- mvc/views/helpers/fichealbum.php
+++ mvc/views/helpers/fichealbum.php
@@ -1,434 +1,274 @@
 <?php
 
 class FicheAlbum {
-
+    
     public function small($o_tome, $getUrlSerie = true) {
-
+        
         if (is_array($o_tome)) {
-
             $o_tome = (object) $o_tome;
         }
 
-
-
         $html = '
-
-<div class="cadre1 fiche_small">
-
-	<div style="float:left" class="mw50">
-
-		' . $this->urlAlbum($o_tome, 'couvSmall') . '
-
-	</div>
-
-    <div style="float:left;font-size:0.9em" class="mw50">
-
-	';
-
-
-
-
-
+            <div class="cadre1 fiche_small">
+            <div style="float:left" class="mw50">
+            ' . $this->urlAlbum($o_tome, 'couvSmall') . '
+            </div>
+            <div style="float:left;font-size:0.9em" class="mw50">';
+        
         // titre de l'album
-
         if ($o_tome->TITRE_TOME) {
-
-
-
             if ($o_tome->NUM_TOME AND !(stristr($o_tome->TITRE_TOME, $o_tome->NUM_TOME))) {
-
                 $html .= '<i>tome ' . $o_tome->NUM_TOME . ' -</i> ';
             }
-
-            $html .=$this->urlAlbum($o_tome, 'albTitle') . '<br>';
+            $html .= $this->urlAlbum($o_tome, 'albTitle') . '<br>';
         }
-// nom de la serie
-
-        if ($o_tome->NOM_SERIE AND ($o_tome->TITRE_TOME AND (strtolower($o_tome->NOM_SERIE) != strtolower($o_tome->TITRE_TOME)) AND $getUrlSerie)) {
-
+        
+        // nom de la serie
+        if ($o_tome->NOM_SERIE AND $o_tome->TITRE_TOME AND $getUrlSerie
+            AND (strtolower($o_tome->NOM_SERIE) != strtolower($o_tome->TITRE_TOME)) ) {
             $html .= 'Série : ' . $this->urlSerie($o_tome) . '';
         }
+        
         // genre
-
-
-
-
-        $html .= '
-
-	</div>
-
-	<hr class="expSep">
-
-</div>';
-
+        $html .= '</div><hr class="expSep"></div>';
         return $html;
     }
-
+        
     public function medium($o_tome, $getUrlSerie = true) {
-
         if (is_array($o_tome)) {
-
             $o_tome = (object) $o_tome;
         }
 
         $html = '
-
-<div class="cadre1 fiche_medium">
-
-	<div style="float:left">
-
-		' . $this->urlAlbum($o_tome, 'couvMedium') . '
-
-	</div>
-
-    <div style="float:left">
-
-	<table>';
-
-
-
-
-
+            <div class="cadre1 fiche_medium">
+            <div style="float:left">
+            ' . $this->urlAlbum($o_tome, 'couvMedium') . '
+            </div>
+            <div style="float:left">';
+        $html .= '<table>';
+        
         // titre de l'album
-
         if ($o_tome->TITRE_TOME) {
-
             $html .= '<tr><td>Titre : </td><td>';
-
             if ($o_tome->NUM_TOME AND !(stristr($o_tome->TITRE_TOME, $o_tome->NUM_TOME))) {
-
                 $html .= '<i>tome ' . $o_tome->NUM_TOME . ' -</i> ';
             }
-
             $html .=$this->urlAlbum($o_tome, 'albTitle') . '</td></tr>';
         }
-
+        
         // nom de la serie
-
         if ($o_tome->NOM_SERIE AND ($o_tome->TITRE_TOME AND (strtolower($o_tome->NOM_SERIE) != strtolower($o_tome->TITRE_TOME)) AND $getUrlSerie)) {
-
             $html .= '<tr><td>Série : </td><td>' . $this->urlSerie($o_tome) . '</td></tr>';
         }
 
-        $html .= '</table>
-
-	</div>
-
-	<hr class="expSep">
-
-</div>';
+        $html .= '</table>';
+        $html .= '</div><hr class="expSep"></div>';
 
         return $html;
     }
-
+    
     public function big($o_tome, $sep = true, $getUrlSerie = true) {
-
         if (is_array($o_tome)) {
-
             $o_tome = (object) $o_tome;
         }
 
         $html = '
-
-<div class="cadre1 fiche_big">
-
-	<div style="float:left" class="mw50">
-
-		' . $this->urlAlbum($o_tome, 'couvBig') . '
-
-	</div>
-
-    <div style="float:left;margin-left: 5px;" class="mw50">
-
-	';
-
-
-
-
+            <div class="cadre1 fiche_big">
+            <div style="float:left" class="mw50">
+            ' . $this->urlAlbum($o_tome, 'couvBig') . '
+            </div>
+            <div style="float:left;margin-left: 5px;" class="mw50">';
 
         // titre de l'album
-
         if ($o_tome->TITRE_TOME) {
-
-
-
             if ($o_tome->NUM_TOME AND !(stristr($o_tome->TITRE_TOME, $o_tome->NUM_TOME))) {
-
                 $html .= '<i>T' . $o_tome->NUM_TOME . ' - </i> ';
             }
-
             $html .= "<strong>" . $this->urlAlbum($o_tome, 'albTitle') . '</strong><br>';
         }
+
         // note/votes
-
         if ($o_tome->NB_NOTE_TOME > 0) {
-
             $html .= '<div align="center" id=noteTome' . $o_tome->ID_TOME . '> </div>';
             $html .= "<script>$('#noteTome" . $o_tome->ID_TOME . "').raty({score: " . $o_tome->MOYENNE_NOTE_TOME / 2 . ", readOnly: true});</script>";
         }
-
+        
         // nom de la serie
         $html.="<p class='fiche_album'>";
         if ($o_tome->NOM_SERIE AND ($o_tome->TITRE_TOME AND (strtolower($o_tome->NOM_SERIE) != strtolower($o_tome->TITRE_TOME)) AND $getUrlSerie)) {
-
             $html .= 'Série : ' . $this->urlSerie($o_tome) . '<br>';
         }
+        
         // genre
-
         if ($o_tome->NOM_GENRE) {
-
             $html .= 'Genre : ';
-
             $html .= '<i>' . $o_tome->NOM_GENRE . '</i><br>';
         }
-
         if ($o_tome->scpseudo) {
-
             $html .= 'Auteur(s) : ';
-
             $html .= '<i>' . $o_tome->scpseudo . ($o_tome->ID_SCENAR == $o_tome->ID_DESSIN ? '' : " / " . $o_tome->depseudo ) . '</i><br>';
         }
-
+        
         // editeur
-
         if ($o_tome->NOM_EDITEUR) {
-
             $html .= 'Editeur : ';
-
             $html .= '<i>' . $o_tome->NOM_EDITEUR . '</i>';
             if (($o_tome->NOM_COLLECTION) and ($o_tome->NOM_COLLECTION != '<N/A>')) {
-
                 $html .= ' -  <i>' . $o_tome->NOM_COLLECTION . '</i>';
             }
-            $html.="<br>";
+            $html .= "<br>";
         }
-
-
-
+        
         // collection editeur
         // date de parution
-
         if ($o_tome->DATE_PARUTION_EDITION) {
-
             $html .= 'Date parution : ';
-
-            $html .=$this->dateParution($o_tome->DATE_PARUTION_EDITION);
+            $html .= $this->dateParution($o_tome->DATE_PARUTION_EDITION);
         }
 
-        $html.="</p>";
-
+        $html .= "</p>";
+        
         if (Bdo_Cfg::user()->minAccessLevel(2)) {
             // ajout des liens d'ajout dans la collection
             $html .= '<br>' . $this->linkCollection($o_tome);
         }
 
-
-        $html .= '
-	</div>';
-
-
+        $html .= '</div>';
+        
         if ($sep) {
             $html .= '<hr class="expSep">';
         }
-        $html .= '
-        </div>';
+        $html .= '</div>';
 
         return $html;
     }
-
+    
     public function serie($o_serie) {
-
         if (is_array($o_serie)) {
-
             $o_serie = (object) $o_serie;
         }
 
-        $html = '
-
-<div class="cadre1">';
-
+        $html = '<div class="cadre1">';
 
         if ($o_serie->IMG_COUV_SERIE)
-            $html.= '<img src="' . BDO_URL_COUV . $o_serie->IMG_COUV_SERIE . '" class="couvSmall" style="float:left;margin=2px">';
-        $html.='<div><table>
+            $html .= '<img src="' . BDO_URL_COUV . $o_serie->IMG_COUV_SERIE . '" class="couvSmall" style="float:left;margin=2px">';
 
-		<tr><td class="fiche_h2l">' . $this->urlSerie($o_serie) . '</td></tr>';
+        $html .= '<div><table><tr><td class="fiche_h2l">';
+        $html .= $this->urlSerie($o_serie);
+        $html .= '</td></tr>';
 
         // genre
-
         if ($o_serie->NOM_GENRE) {
-
             $html .= '<tr><td class="fiche_h3l">Genre : <i>' . $o_serie->NOM_GENRE . '</i></td></tr>';
         }
 
         //avancement 0: Finie, 1: En cours, 2: One-shot, 3:Interrompue
-
-
-
         if (!is_null($o_serie->FLG_FINI_SERIE)) {
-
             $a_avancement = array(
                 0 => 'Finie',
                 1 => 'En cours',
                 2 => 'One-shot',
                 3 => 'Interrompue',
             );
-
-
-
             $html .= '<tr><td class="fiche_h3l">Avancement : ' . $a_avancement[$o_serie->FLG_FINI_SERIE] . '</td></tr>';
         }
-
-
-
-
-
-
-
         $html .= '</table>';
-
-
-
-        $html .= '</div><hr class="expSep" />
-            
-</div>';
+        $html .= '</div><hr class="expSep" />';
+        $html .= '</div>';
 
         return $html;
     }
-
+    
     /*
-
      * urlAlbum
-
      * fournit l'url d'un lien vers la iframe album
-
      *
-
      */
-
     public function urlAlbum($o_tome, $class = 'couvBig', $is_edition = false)
     /*
      *  Fonction de construction d'une url d'un album
-     * Si la variable $is_edition = true, on ajoute l'id édition dans les liens
+     *  Si la variable $is_edition = true, on ajoute l'id édition dans les liens
      *  Utile pour l'accès depuis la collection de l'utilisateur, où on sélectionne une édition précise
-     */ {
-
+     */ 
+    {
         if (is_array($o_tome)) {
-
             $o_tome = (object) $o_tome;
         }
 
         $id_link = "Album?id_tome=" . $o_tome->ID_TOME;
+
         if ($is_edition)
             $id_link.="&id_edition=" . $o_tome->ID_EDITION;
-
+        
         // couverture par defaut
-
         if (!$o_tome->IMG_COUV)
             $o_tome->IMG_COUV = "default.png";
 
-
-
         $x = getenv("HTTP_USER_AGENT");
-
+        
         if (strpos($x, 'MSIE 7.0') || strpos($x, 'MSIE 6.0')) {
-
             return '#" onclick="window.open(' . "'" . BDO_URL . $id_link . "','Album','width=600,height=600,scrollbars=1')" . ';return false;';
         } else {
-
-
-
             // titre html
-
             if ($o_tome->NOM_SERIE AND (strtolower($o_tome->NOM_SERIE) != strtolower($o_tome->TITRE_TOME))) {
-
                 $titleHtml = $o_tome->NOM_SERIE;
 
                 if ($o_tome->NUM_TOME AND !(stristr($o_tome->TITRE_TOME, $o_tome->NUM_TOME))) {
-
                     $titleHtml .= ' - ' . 'tome ' . $o_tome->NUM_TOME;
                 }
 
                 $titleHtml .= ' - ' . $o_tome->TITRE_TOME;
             } else {
-
                 $titleHtml = $o_tome->TITRE_TOME;
             }
 
-
-
             $html = '<a class="fancybox fancybox.iframe {width:600,height:500}"
-
                 href="' . BDO_URL . $id_link . '" title="' . $titleHtml . '">';
-
+            
             switch ($class) {
-
                 case "couvBig": {
-
                         $html .= '<img src="' . BDO_URL_COUV . $o_tome->IMG_COUV . '" class="' . $class . '" title="' . $titleHtml . '"/>';
-
                         $html .= '</a>' . ($o_tome->NOM_EDITEUR ? '<div class="copyright">&copy; ' . $o_tome->NOM_EDITEUR . '</div>' : '');
-
                         break;
-                    }
+                }
 
                 case "couvMedium": {
-
                         $html .= '<img src="' . BDO_URL_COUV . $o_tome->IMG_COUV . '" class="' . $class . '" title="' . $titleHtml . '"/>';
-
                         $html .= '</a>' . ($o_tome->NOM_EDITEUR ? '<div class="copyright">&copy; ' . $o_tome->NOM_EDITEUR . '</div>' : '');
-
                         break;
-                    }
+                }
 
                 case "couvSmall": {
-
                         $html .= '<img src="' . BDO_URL_COUV . $o_tome->IMG_COUV . '" class="' . $class . '" title="' . $titleHtml . '"/>';
-
                         $html .= '</a>';
-
                         break;
-                    }
-
+                }
                 case "albTitle": {
-
                         $html .= $o_tome->TITRE_TOME . '</a>';
-
                         if (Bdo_Cfg::user()->minAccessLevel(1)) {
-
                             $html .= '&nbsp;&nbsp;<a href="' . '#' . '" ><img src="' . BDO_URL_IMAGE . 'edit.gif" border=0></a>';
                         }
-
                         break;
-                    }
-
+                }
                 default: {
-
                         $html .= '<img src="' . BDO_URL_COUV . $o_tome->IMG_COUV . '" title="' . $titleHtml . '"/>';
-
                         $html .= '</a>' . ($o_tome->NOM_EDITEUR ? '<div class="copyright">&copy; ' . $o_tome->NOM_EDITEUR . '</div>' : '');
-
                         break;
-                    }
+                }
             }
-
             return $html;
         }
     }
-
+    
     public function urlSerie($o_serie) {
-
         if (is_array($o_serie)) {
-
             $o_serie = (object) $o_serie;
         }
 
         $html = '<a href="' . BDO_URL . 'SerieBD?id_serie=' . $o_serie->ID_SERIE . '" title="' . $o_serie->NOM_SERIE . '">
-
-           ' . $o_serie->NOM_SERIE . '</a>';
+            ' . $o_serie->NOM_SERIE . '</a>';
 
         if (Bdo_Cfg::user()->minAccessLevel(1)) {
-
             $html .= '&nbsp;&nbsp;<a href="' . '#' . '"><img src="' . BDO_URL_IMAGE . 'edit.gif" border=0></a>';
         }
 
@@ -442,6 +282,7 @@ class FicheAlbum {
         if (User::minAccesslevel(2)) {
             $id_source = "addAlbum" . $o_tome->ID_EDITION;
             $html = "<div id='" . $id_source . "' style='font-size:0.9em;'>";
+
             if ($o_tome->DATE_AJOUT) {
                 if ($o_tome->FLG_ACHAT == "O") {
                     $html.= "ajouté à vos futurs achats le " . date_format(date_create($o_tome->DATE_AJOUT), "d/m/Y");
@@ -455,15 +296,19 @@ class FicheAlbum {
                 $html .= "<a class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only' href='javascript:addAlbum(" . $o_tome->ID_TOME . "," . $o_tome->ID_EDITION . ',"N")' . "'" . ' title="Ajouter cet album dans votre collection">Dans ma collection</a>';
                 $html .= " - <a class='ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only' href='javascript:addAlbum(" . $o_tome->ID_TOME . "," . $o_tome->ID_EDITION . ',"O")' . "'" . ' title="A acheter prochainement">Futur Achat</a>';
             }
+
             $html.= "</div>";
+
         } else {
             $html = "<i>Connectez vous pour ajouter cet album dans votre collection !</i>";
         }
+
         return $html;
     }
 
     public function getSponsor($o_tome) {
         $html = "<a href='";
+
         if ($o_tome->ISBN_EDITION) {
             $html .= "http://www.amazon.fr/exec/obidos/ASIN/" . $o_tome->ISBN_EDITION . "/bdovorecom-21/";
         } else if ($o_tome->EAN_EDITION) {
@@ -471,25 +316,21 @@ class FicheAlbum {
         } else {
             $html .= "http://www.amazon.fr/exec/obidos/external-search?tag=bdovorecom-21&keyword=" . htmlspecialchars(stripslashes($o_tome->TITRE_TOME)) . "&mode=books-fr";
         }
-        $html .="' target='_blank'><img src='" . BDO_URL_IMAGE . "amazon.gif'></a>";
+        $html .= "' target='_blank'><img src='" . BDO_URL_IMAGE . "amazon.gif'></a>";
+
         return $html;
     }
 
     public function dateParution($date, $flag = 0) {
 
         if (!empty($date)) {
-
             // date de parution superieur a date du jour moins 1 mois
-
             if (TimestampDate($date) > (time() - 2592000)) {
-
                 $dte_parution = translate_date($date);
             } else {
-
                 $dte_parution = month_to_text(substr($date, 5, 2)) . " " . substr($date, 0, 4);
             }
         } else {
-
             if ($flag == 1)
                 $dte_parution = "Date introuvable";
             else
@@ -498,5 +339,4 @@ class FicheAlbum {
 
         return $dte_parution;
     }
-
 }
