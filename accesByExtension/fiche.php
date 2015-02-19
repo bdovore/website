09ab9c18ec0b9e13a6224c0def8dfe6248adad9a
Chninkel <chninkel@bdovore.com>
2015-02-19 21:34:41 +0100

    typo dans le lien vers fiche album (balise [BD] du forum)

--- accesByExtension/fiche.php
+++ accesByExtension/fiche.php
@@ -1,4 +1,3 @@
 <?php
-
 header('Status: 301 Moved Permanently', false, 301);  
-header('Location: '.BDO_URL."alum/fiche?id_tome=".$_GET["alb_id"]);
+header('Location: '.BDO_URL."album/fiche?id_tome=".$_GET["alb_id"]);
