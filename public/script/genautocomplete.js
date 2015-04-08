339fd336d9324a06d21bb96f7d2effe4fbf80452
Chninkel <chninkel@bdovore.com>
2015-04-08 17:52:49 +0200

    corrige les alignements

--- public/script/genautocomplete.js
+++ public/script/genautocomplete.js
@@ -5,54 +5,53 @@ function addAutocomplete (idInput, idSpan, idHiddenField, url, min){
 *   remplit le champ hidden avec l'id dans idhidden
 */      
     
-        if (! min) {
-           var min = 2;
-        }
-        var cache = {};
-        $( "#"+idInput ).autocomplete({
-        minLength: min,
-        source: function( request, response ) {
-            var term = request.term;
-            if ( term in cache ) {
-            response( cache[ term ] );
-            return;
-            }
-            $.getJSON( url, request, function( data, status, xhr ) {
-            cache[ term ] = data;
-            response( data );
-            });
-        },
-        select: function( event, ui ) {
-            $("#"+idHiddenField).val(ui.item.id);
-            $("#"+idSpan).text(ui.item.id);
-            
-        }
-        });
+    if (! min) {
+       var min = 2;
     }
-    
-    function addSelectOption(idInput,url,id_select){
-        
-        
-        $("#"+idInput).empty();
-        $.getJSON( url, function( data) {
-            
-           $.each(data, function( item ) {
-               
-               if (id_select == data[item].id) {
-                   select = "selected";
-               }
-               else {
-                   select = "";
-               }
-               $("#"+idInput).append("<option value='"+data[item].id+"'"+ select + ">"+htmlEscape(data[item].label)+"</option>")
-           });
-           }); 
+    var cache = {};
+    $( "#"+idInput ).autocomplete({
+    minLength: min,
+    source: function( request, response ) {
+        var term = request.term;
+        if ( term in cache ) {
+        response( cache[ term ] );
+        return;
         }
+        $.getJSON( url, request, function( data, status, xhr ) {
+        cache[ term ] = data;
+        response( data );
+        });
+    },
+    select: function( event, ui ) {
+        $("#"+idHiddenField).val(ui.item.id);
+        $("#"+idSpan).text(ui.item.id);
+        
+    }
+    });
+}
+    
+function addSelectOption(idInput,url,id_select){
+    
+    $("#"+idInput).empty();
+    $.getJSON( url, function( data) {
+        
+       $.each(data, function( item ) {
+           
+           if (id_select == data[item].id) {
+               select = "selected";
+           }
+           else {
+               select = "";
+           }
+           $("#"+idInput).append("<option value='"+data[item].id+"'"+ select + ">"+htmlEscape(data[item].label)+"</option>")
+       });
+       }); 
+    }
 function htmlEscape(str) {
-    return String(str)
-            .replace(/&/g, '&amp;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
+return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
 }
