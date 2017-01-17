96161926f8a0206f1dea65dd7e8f1d245d90225f
Félix Sipma <felix.sipma@no-log.org>
2017-01-17 14:51:13 +0100

    Utilisation de BDO_URL_FORUM à la place de http://forum.bdovore.com/

--- accesByExtension/forum/viewtopic.php
+++ accesByExtension/forum/viewtopic.php
@@ -1,4 +1,4 @@
 <?php
 header('Status: 301 Moved Permanently', false, 301);
-header('Location: http://forum.bdovore.com/viewtopic.php?'.$_SERVER['QUERY_STRING']);
+header('Location: ' . BDO_URL_FORUM . 'viewtopic.php?'.$_SERVER['QUERY_STRING']);
 ?>
