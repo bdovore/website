188ef2e72720887c168cf4eec2bee2c8a0dd7229
tom <tom@tom-Studio-1737>
2017-04-30 12:30:53 +0200

    AdminAuteur : ajout de la recherche

--- mvc/controllers/Adminauteur.php
+++ mvc/controllers/Adminauteur.php
@@ -16,19 +16,25 @@ class Adminauteur extends Bdo_Controller {
         $this->loadModel("Auteur");
         
         $filter = getVal("filter","0");
+        $search_filter = getVal("l_search","");
         $query = ""; 
+         if ($search_filter)  {
+            $query .= " WHERE pseudo like '%".$search_filter."%' ";
+        }
         if ($filter == "1") {
-            $query .= " WHERE COMMENT = '' OR COMMENT IS NULL ";
+            $query .= " AND (COMMENT = '' OR COMMENT IS NULL ) ";
         }
         if ($filter == "2") {
-            $query .= " WHERE FLG_SCENAR <> 1 AND"
-                    . " FLG_DESSIN <> 1 AND FLG_COLOR <> 1  ";
+            $query .= " AND (FLG_SCENAR <> 1 AND"
+                    . " FLG_DESSIN <> 1 AND FLG_COLOR <> 1  ) ";
         }
+       
         $query .= " ORDER BY VALID_DTE DESC LIMIT 0, 100";
         $dbs_auteur = $this->Auteur->load("c",$query);
         $this->view->set_var("dbs_auteur", $dbs_auteur);
         $this->view->set_var("filter", $filter);
         $this->view->set_var("PAGETITLE", "Administration Bdovore - Auteur");
+        $this->view->set_var("searchvalue", $search_filter);
         $this->view->render();
     }
 }
