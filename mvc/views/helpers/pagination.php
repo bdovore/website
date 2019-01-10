3e63fda611bd6847bbc71f4acb2e6133da01587c
tom <tom@tom-Studio-1737>
2019-01-10 18:16:58 +0100

    Menu Proposition plus petit pour que ça entre

--- mvc/views/helpers/pagination.php
+++ mvc/views/helpers/pagination.php
@@ -161,7 +161,7 @@ class Pagination{
 
         // tableaux contenant les liens d'accès et le texte à afficher
         $tab_menu_lien = array( "macollection", "macollection/mesetageres", "macollection/messeries", "macollection/mesauteurs", "macollection/futursachats", "macollection/monactu", "macollection/seriecomplete", "macollection/addition", "macollection/proposition","macollection/statistiques");
-        $tab_menu_texte = array( "Accueil", "Mes Albums", "Mes Séries", "Mes Auteurs", "Futurs Achats", "Mon Actualité", "Séries à compléter", "L'Addition","Mes propositions", "Stats" );
+        $tab_menu_texte = array( "Accueil", "Mes Albums", "Mes Séries", "Mes Auteurs", "Futurs Achats", "Mon Actualité", "Séries à compléter", "L'Addition","Propositions", "Stats" );
 
         // informations sur la page
 
